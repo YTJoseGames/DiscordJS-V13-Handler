d968d239ffcfab9b2b85a1e7a591c4c89f3a8598
YTJoseGames <68444005+YTJoseGames@users.noreply.github.com>
2022-08-31 21:27:20 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,20 @@
+const { Client, Collection } = require('discord.js')
+const config = require('./configs/config.json')
+const fs = require('fs')
+
+const client = new Client({
+    intents: 32767,
+    partials: ['MESSAGE', 'CHANNEL', 'REACTION', 'USER', 'GUILD_MEMBER']
+});
+
+client.commands = new Collection()
+client.aliases = new Collection()
+client.prefix = config.prefix
+module.exports = client;
+client.categories = fs.readdirSync("./commands/");
+
+["command", "events"].forEach(handler => {
+    require(`./handlers/${handler}`)(client);
+});
+
+client.login(config.token)
